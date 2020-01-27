8b26e067116c3bab2eeb73ff5d3adcc193f9405b
gomsang <gyeongrok.kim@gomsang.com>
2020-01-28 00:44:29 +0900

    initial commit

---
+++ app/src/main/java/com/gomsang/lab/sparkling/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gomsang.lab.sparkling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
